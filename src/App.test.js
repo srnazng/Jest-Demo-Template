2b1dcf42475b9c554afde46863877135aaf34ac2
srnazng <srnazng@gmail.com>
2021-05-20 21:19:09 -0400

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,21 @@
 import App from './App';
+import Counter from './components/Counter';
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 it("renders without crashing", () => {
   shallow(<App />);
+});
+
+it("renders correctly", () => {
+  const wrapper = shallow(<App />);
+  // const wrapper = mount(<App />); // also works but less efficient
+  expect(wrapper.contains(<Counter />)).toBe(true);
+});
+
+// shallow vs. mount
+it("contains header", () => {
+  // const wrapper = shallow(<App />); // does NOT work because does not render child components
+  const wrapper = mount(<App />);
+  expect(wrapper.contains(<h4>Counter</h4>)).toBe(true);
 });
