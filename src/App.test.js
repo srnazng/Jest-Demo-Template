955ed0bc9628beecd8f5aa083efe46110afff7c2
Serena Zeng <srnazng@gmail.com>
2021-05-23 16:48:16 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,16 +6,3 @@ import { shallow, mount } from "enzyme";
 it("renders without crashing", () => {
   shallow(<App />);
 });
-
-it("renders correctly", () => {
-  const wrapper = shallow(<App />);
-  // const wrapper = mount(<App />); // also works but less efficient
-  expect(wrapper.contains(<Counter />)).toBe(true);
-});
-
-// shallow vs. mount
-it("contains header", () => {
-  // const wrapper = shallow(<App />); // does NOT work because does not render child components
-  const wrapper = mount(<App />);
-  expect(wrapper.contains(<h4>Counter</h4>)).toBe(true);
-});
